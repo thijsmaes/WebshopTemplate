165fafefdc5b65cacd086a02d16dcec61a22ea2d
tma <thijs.maes@soprasteria.com>
2015-10-05 23:00:54 +0200

    5/10

--- webshoptemplate-backend/src/main/java/be/webshop/templatewebshop/backend/entities/Client.java
+++ webshoptemplate-backend/src/main/java/be/webshop/templatewebshop/backend/entities/Client.java
@@ -1,7 +1,21 @@
 package be.webshop.templatewebshop.backend.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 /**
  * Created by tmaes on 15/08/2015.
  */
+@Entity
 public class Client extends Person {
+    private String id;
+
+    @Id
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
